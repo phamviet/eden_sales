83d23019c4f570c12e4c72c1e2c98059b6763234
Viet Pham <phpcodervn@gmail.com>
2017-03-03 22:41:56 +0700

    looking for customer profile in a wrong company

--- eden_sales/sales_order.py
+++ eden_sales/sales_order.py
@@ -5,17 +5,22 @@ import frappe
 
 
 def set_missing_values(doc, method):
-	if doc.company:
-		customer_profile = frappe.get_value("Company", doc.company, "customer_profile")
-		if not customer_profile:
-			frappe.throw("Customer profile is mandatory for company <strong>{}</strong>".format(doc.company))
+	if not doc.po_no:
+		return
 
-		customer = frappe.get_doc("Customer", customer_profile)
-		doc.customer = customer_profile
-		doc.customer_name = customer.customer_name
+	original_company = frappe.get_value("Purchase Order", doc.po_no, "company")
+	customer_profile = frappe.get_value("Company", original_company, "customer_profile")
 
-		doc.customer_address = frappe.get_value("Address", {"customer": doc.customer_name, "is_primary_address": 1})
+	if not customer_profile:
+		frappe.throw("Customer profile is mandatory for company <strong>{}</strong>".format(original_company))
 
-		default_price_list = frappe.get_value("Customer", customer_profile, "default_price_list")
-		if default_price_list:
-			doc.selling_price_list = default_price_list
+	customer = frappe.get_doc("Customer", customer_profile)
+	doc.customer = customer_profile
+	doc.customer_name = customer.customer_name
+
+	doc.customer_address = frappe.get_value("Address", {"customer": doc.customer_name, "is_primary_address": 1})
+
+	default_price_list = frappe.get_value("Customer", customer_profile, "default_price_list")
+	if default_price_list:
+		doc.selling_price_list = default_price_list
+
