21511b937a28658a9ec4aa782dc782fda74ca67f
Viet Pham <phpcodervn@gmail.com>
2017-03-20 22:48:10 +0700

    Remove Delivery Note hooks

--- eden_sales/hooks.py
+++ eden_sales/hooks.py
@@ -40,76 +40,11 @@ doctype_js = {
 	"Sales Invoice": "scripts/sales_invoice.js",
 }
 
-# Includes in <head>
-# ------------------
-
-# include js, css files in header of desk.html
-# app_include_css = "/assets/eden_sales/css/eden_sales.css"
-# app_include_js = "/assets/eden_sales/js/eden_sales.js"
-
-# include js, css files in header of web template
-# web_include_css = "/assets/eden_sales/css/eden_sales.css"
-# web_include_js = "/assets/eden_sales/js/eden_sales.js"
-
-# Home Pages
-# ----------
-
-# application home page (will override Website Settings)
-# home_page = "login"
-
-# website user home page (by Role)
-# role_home_page = {
-#	"Role": "home_page"
-# }
-
-# Website user home page (by function)
-# get_website_user_home_page = "eden_sales.utils.get_home_page"
-
-# Generators
-# ----------
-
-# automatically create page for each record of this doctype
-# website_generators = ["Web Page"]
-
-# Installation
-# ------------
-
-# before_install = "eden_sales.install.before_install"
-# after_install = "eden_sales.install.after_install"
-
-# Desk Notifications
-# ------------------
-# See frappe.core.notifications.get_notification_config
-
-# notification_config = "eden_sales.notifications.get_notification_config"
-
-# Permissions
-# -----------
-# Permissions evaluated in scripted ways
-
-# permission_query_conditions = {
-# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
-# }
-#
-# has_permission = {
-# 	"Event": "frappe.desk.doctype.event.event.has_permission",
-# }
-
-# Document Events
-# ---------------
-# Hook on document methods and events
-
 doc_events = {
 
 	"Sales Order": {
 		"set_missing_values": [
 			"eden_sales.sales_order.set_missing_values"
-		]
-	},
-
-	"Delivery Note": {
-		"on_submit": [
-			"eden_sales.delivery_note.on_submit"
 		]
 	}
 }
